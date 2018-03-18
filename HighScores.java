bccc6a81757e210cdfbd18361378dce989a8baa3
VaclavBartunek <37154252+VaclavBartunek@users.noreply.github.com>
2018-03-18 20:01:55 +0000

    commit 3
    Stores score and name into vector, print score, on the game board,

--- HighScores.java
+++ HighScores.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;    //action listeners
 import java.awt.*;
 import java.io.*;
+import java.util.Vector; // import vectors
 
 public class HighScores implements ActionListener { // specification of class
 		
@@ -11,21 +12,19 @@ public class HighScores implements ActionListener { // specification of class
 		
 		
 	private JFrame f1 = new JFrame("HIGH SCORES");   //create a blank window
-	JPanel p1 = new JPanel();
+	private JPanel p1 = new JPanel();
+	private Vector <String> namesVector  = new Vector <String>(10,2); // if more then 10 args then *2 spaces
+	private Vector <Long> scoresVector  = new Vector <Long>(10,2);
 	
+	private GridLayout myLayout = new GridLayout(11,2); 
 	
+	private	JLabel l1 = new  JLabel("hello");
+	private JLabel l2 = new  JLabel("hello");
+	private JLabel l3 = new  JLabel("hello");
+	private JLabel l4 = new  JLabel("hello");
 	
-	GridLayout myLayout = new GridLayout(11,2); 
-	
-
-	JLabel l1 = new  JLabel("hello");
-	JLabel l2 = new  JLabel("hello");
-	JLabel l3 = new  JLabel("hello");
-	JLabel l4 = new  JLabel("hello");
-	
-	
-	JLabel l11 = new  JLabel("YOUR NAME");
-	JTextField tf1= new JTextField("Type your name"); 
+	private JLabel l11 = new  JLabel("YOUR NAME");
+	private JTextField nameField= new JTextField("Type your name"); 
 	
 	public HighScores() //constructor
 	{
@@ -48,24 +47,79 @@ public class HighScores implements ActionListener { // specification of class
 		p1.add(l4);
 		
 		p1.add(l11);
-		p1.add(tf1);
+		p1.add(nameField);
 		
-		
-		tf1.addActionListener(this);
+		nameField.addActionListener(this);
 	}
+	
+	
+	
+	
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		System.out.println("The text entered is :["+ tf1.getText()+"]");	
+		namesVector.add(new String(nameField.getText()));	// storing name into vector
+		scoresVector.add(new Long(score));	// store score into vector
+	
+	
+
+	//	System.out.println("The text entered is :["+ nameField.getText()+"]");	
+	//  System.out.println("Vector Name 0  is "+ namesVector.get(0) );
+	//	System.out.println("Vector Name 1  is "+ namesVector.get(1) );
+	//	System.out.println("Vector Score 0  is "+ scoresVector.get(0) );
+	//	System.out.println("Vector Score 1  is "+ scoresVector.get(1) );
+	//	System.out.println("Are they same" + nameField.getText() == namesVector.get(0));
+
+
+
+			// searching for the same name ???
+		/*for(int i=0;i<10;i++)
+		{
+
+		if(nameField.getText() == namesVector.get(i))
+			{
+				System.out.println( i + " is position of same name \n" ); 
+				System.out.println( nameField.getText() == namesVector.get(i));
+				break; 
+			}
+		} */
+		
+	/*	
+		// searching for the smallest score value
+		for(int i=0;i<10;i++)
+		{
+			//System.out.println("Vector Score  is "+ scoresVector.get(i) );
+			if ( score <= scoresVector.get(i))
+			{
+				System.out.println("Score Vector number "+ i + " is "+ scoresVector.get(i) + " and should be pushed" );
+				long temp = scoresVector.get(i);
+				scoresVector.set(i,score);    //sets current score to i position 
+				temp2= scoresVector(i+1) ;
+				ScoresVector(i+1)= temp;
+				
+			}
+			
+		}
+		
+			*/ 
+		 
+		 
+		// writing into file
 		try{
 			FileWriter fw = new FileWriter("Storage.txt",true);
-			fw.write(tf1.getText()+"\n");
+			fw.write(nameField.getText()+"\n");
+			fw.write( score +"\n");
 			fw.close();
+				
+			
 		}
 		catch(IOException ex){
 			System.out.println("Did not find the file");	
 		}
 	}
+	
+	
+	
 	
 	public int getScore()
 	{
@@ -75,9 +129,11 @@ public class HighScores implements ActionListener { // specification of class
 	public void incrementScore()
 	{
 		score=score+1;
-		//System.out.println("Your score is" + score);	
+		System.out.println("Your score is" + score);	
 	}		
 		
-		
+	
+	
+	 // if (gameFinished)
 		 
 }
